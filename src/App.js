6f7c1f3b342a73487b07e5a4572ff18bf30620c0
王东(10032047) <wangd-aa@glodon.com>
2020-11-07 19:28:32 +0800

    quick start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Header = () => (
+	<ul>
+		<li>
+			<Link to="/">Home</Link>
+		</li>
+		<li>
+			<Link to="/about">About</Link>
+		</li>
+		<li>
+			<Link to="/topics">Topics</Link>
+		</li>
+	</ul>
+);
+
+const Home = () => <h2>Home</h2>;
+const About = (props) => {
+	console.log(props);
+	return <h2>About</h2>;
+};
+const Topic = (props) => {
+	console.log('match11', props.match, props);
+	return (
+		<h3>
+			Requested param:{props.match.params.id}
+			{/* <Link to={`${match.url}/about`}>aaaabbbbooouuuttt</Link>
+			<Route path={`${match.path}/:asdf`} component={About} /> */}
+		</h3>
+	);
+};
+const Topics = ({ match }) => {
+	console.log('match', match);
+	return (
+		<div>
+			<h2>Topics</h2>
+			<ul>
+				<li>
+					<Link to={`${match.url}/components`}>components</Link>
+				</li>
+				<li>
+					<Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+				</li>
+			</ul>
+			<Route
+				exact
+				path={match.path}
+				render={() => <h3>Please select a topic.</h3>}
+			/>
+			<Route path={`${match.path}/:id`} component={Topic} />
+		</div>
+	);
+};
+
+const App = () => (
+	<Router>
+		<div>
+			<Header />
+			<Route exact path="/" component={Home} />
+			<Route path="/about" component={About} />
+			<Route path="/topics" component={Topics} />
+		</div>
+	</Router>
+);
 
 export default App;
